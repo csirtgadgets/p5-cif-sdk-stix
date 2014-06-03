c1c0bf14bf2f5e261f40d4b9bf5b33b63a3b9269
Wes Young <wes@barely3am.com>
2014-06-03 13:51:37 -0400

    ...

---
+++ lib/CIF/SDK/Stix.pm
@@ -0,0 +1,41 @@
+package CIF::SDK::Stix;
+
+use strict;
+use warnings;
+
+##TODO
+## https://github.com/akreffett/cif_json2stix/blob/master/cif-json2stix.py
+
+our $VERSION = '0.00_01';
+$VERSION = eval $VERSION;  # see L<perlmodstyle>
+
+=head1 NAME
+
+CIF::SDK::Stix - Perl extension for integrating Stix with CIF
+
+=head1 SYNOPSIS
+
+  use CIF::SDK::Stix;
+
+=head1 DESCRIPTION
+
+Blah blah blah.
+
+=head1 BUGS AND SUPPORT
+
+Please report any bugs at L<http://github.com/csirtgadgets/p5-cif-sdk-stix/issues>.
+
+=head1 AUTHOR
+
+    Wes Young <wes@barely3am.com>
+
+=head1 COPYRIGHT AND LICENSE
+
+Copyright (c) 2014 the CSIRT Gadgets Foundation
+
+This program is free software; you can redistribute it and/or modify it
+under the terms of the the LGPL (v3). 
+
+=cut
+
+1;
